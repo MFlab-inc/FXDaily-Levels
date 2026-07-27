b5c52ca5871b176396de7792753c0bd8be60a976
MFlab-inc <yoshikawa1119@gmail.com>
2026-07-27 12:20:49 +0900

    Update daytrade.js

--- daytrade.js
+++ daytrade.js
@@ -30,6 +30,7 @@ const PAIRS = [
   { code: "NZDUSD", td: "NZD/USD", digits: 5 },
   { code: "AUDNZD", td: "AUD/NZD", digits: 5 },
   { code: "XAUUSD", td: "XAU/USD", digits: 2 },
+  { code: "USOIL",  td: "WTI/USD", digits: 2 },
 ];
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
